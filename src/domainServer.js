cf38b40cdf97211ab2e0aa48280724d5f7a3631a
Treasure <obisiket@yahoo.com>
2023-09-01 12:29:21 +0100

    ..m

--- src/domainServer.js
+++ src/domainServer.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.post("/add-subdomain", async (req, res) => {
   try {
     const { port, subdomain } = {...req.body, ...req.query};
+    console.log({ port, subdomain })
     if (port === undefined || subdomain === undefined) {
       console.log(req.body, req.query);
       return res.status(422).send("invalid port or subdomain");
