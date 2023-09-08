65bd72e1b1a3dafb5ef215f424dc296d91126e42
Treasure <obisiket@yahoo.com>
2023-09-08 20:02:59 +0100

    feat

--- src/proxyServer.js
+++ src/proxyServer.js
@@ -4,6 +4,12 @@ const { client } = require("./helpers");
 
 const proxy = httpProxy.createProxyServer({});
 
+const miniMap = {
+  "one": "http://localhost:3002",
+  "two": "http://localhost:3003",
+  "three": "http://localhost:3004",
+}
+
 const server = http.createServer(async (req, res) => {
   try {
     const host = req.headers.host;
@@ -15,6 +21,8 @@ const server = http.createServer(async (req, res) => {
     
     if (target) {
       proxy.web(req, res, { target });
+    } else if (["one", "two", "three"].includes(subdomain.split('-')[0].replace("ai", ''))) {
+      proxy.web(req, res, { target: miniMap[subdomain.split('-')[0].replace("ai", '')] });
     } else {
       console.log("- Subdomain: " + target);
       res.writeHead(404, { 'Content-Type': 'text/plain' });
